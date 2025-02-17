153484884de35f440fbb7b1b82f6004882be03cc
RianOntu <rianontu13@gmail.com>
2025-02-17 13:53:28 +0600

    added registration feature

---
+++ src/features/auth/authApi.js
@@ -0,0 +1,57 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        const result = await queryFulfilled;
+
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+        dispatch(
+          userLoggedIn({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        const result = await queryFulfilled;
+
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+        dispatch(
+          userLoggedIn({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+      },
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = authApi;
